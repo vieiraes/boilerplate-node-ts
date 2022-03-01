55b2411314808288a6d0f6a471af7ca80bdf88b1
Bruno Vieira <vieira.es@gmail.com>
2022-02-28 22:59:37 -0300

    first commit

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+
+
+export const routes = Router();
+
+
+routes.get('/', (request, response) => {
+
+  return response.json({ message: 'Hello!' })
+})
+
+
---
+++ src/shared/http/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors'
+import { routes } from './routes';
+
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+const port = 3333
+
+
+app.listen(port,()=>{
+
+  console.log(`Server started at ${port}`)
+
+
+})
+
+
+
+
